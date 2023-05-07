fa2eca3dfd7ffbf13cda6c6ce4c0c0d8ef5d99bd
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-05-07 18:57:43 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,75 +1,32 @@
-const dialContainer = document.getElementById('dial-container');
-const dial = document.getElementById('dial');
-const dialMiddle = document.getElementById('dial-middle');
-const dialBackground = document.querySelector('.dial-background');
-
-let dragging = false;
-let angle = 0;
-
-dial.onmousedown = (event) => {
-  event.preventDefault();
-  dragging = true;
-};
-
-document.onmouseup = () => {
-  if (dragging) {
-    setTimeout(() => {
-      dragging = false;
-      angle = 0;
-      dial.style.transform = `rotate(${angle}deg)`;
-      if (dialMiddle.style.display !== 'none') {
-        dialMiddle.style.transform = `rotate(${angle}deg)`;
-      }
-      if (dialBackground.style.display !== 'none') {
-        dialBackground.style.transform = `rotate(${angle}deg)`;
-      }
-    }, 100);
-  }
-};
-
-document.onmousemove = (event) => {
-  if (dragging) {
-    const rect = dial.getBoundingClientRect();
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    angle = Math.atan2(mouseY - centerY, mouseX - centerX) * (180 / Math.PI);
-    dial.style.transform = `rotate(${angle}deg)`;
-    if (dialMiddle.style.display !== 'none') {
-      dialMiddle.style.transform = `rotate(${angle}deg)`;
-    }
-    if (dialBackground.style.display !== 'none') {
-      dialBackground.style.transform = `rotate(${angle}deg)`;
-    }
-  }
-};
-
-let doubleClickTimeout;
-
-dial.ondblclick = () => {
-  if (doubleClickTimeout) {
-    clearTimeout(doubleClickTimeout);
-  }
-  doubleClickTimeout = setTimeout(() => {
-    dialMiddle.style.display = dialMiddle.style.display === 'none' ? 'block' : 'none';
-  }, 300);
-};
-
-dialMiddle.ondblclick = () => {
-  if (doubleClickTimeout) {
-    clearTimeout(doubleClickTimeout);
-  }
-  doubleClickTimeout = setTimeout(() => {
-    dialBackground.style.display = dialBackground.style.display === 'none' ? 'block' : 'none';
-  }, 300);
-};
-
-document.addEventListener('click', (event) => {
-  if (!dialContainer.contains(event.target)) {
-    dialMiddle.style.display = 'none';
-    dialBackground.style.display = 'none';
-  }
-});
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Object Detection</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"></script>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="canvas-container">
+        <video id="video" autoplay playsinline muted></video>
+        <canvas id="canvas"></canvas>
+    </div>
+    <div class="button-container">
+        <button id="switch-camera">
+            <img src="https://raw.githubusercontent.com/aARdeLife/Super-Vision-XR/05a2b3614fce1e3431355210bcf6a71be40b384b/Asset%204.png" alt="Switch Camera" />
+        </button>
+        <button id="speak">
+            <img src="https://raw.githubusercontent.com/aARdeLife/Super-Vision-XR/a59e2c5c95cbba248ed3f801ccb5dd39b7f12a90/Asset%205.png" alt="Speak Detected Object" />
+        </button>
+    </div>
+    <div class="dial-container" id="dial-container">
+        <div class="dial-background" style="background-image: url('https://raw.githubusercontent.com/aARdeLife/Dial/b21d1e78f573072f9cf660639d462405866b7938/yellow.png')"></div>
+        <div class="dial-middle" id="dial-middle" style="background-image: url('https://raw.githubusercontent.com/aARdeLife/Dial/b21d1e78f573072f9cf660639d462405866b7938/orange.png')"></div>
+        <div class="dial" id="dial" style="background-image: url('https://raw.githubusercontent.com/aARdeLife/Dial/b21d1e78f573072f9cf660639d462405866b7938/red.png')"></div>
+    </div>
+    <script src="app.js"></script>
+    <script src="scripts.js"></script>
+</body>
+</html>
